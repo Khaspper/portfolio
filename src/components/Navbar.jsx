cd4773f231373a167c6de46a1003d7e64c86cd4a
Markus <brandonnarciso24@gmail.com>
2023-11-02 23:58:30 -0700

    Fixed logo bug
    now it is the correct size

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,6 +24,7 @@ const Navbar = () => {
         >
           <img src={logo} alt="logo" className='w-9
           h-9 object-contain' />
+          <p className='text-white text-[18px] font-bold cursor-pointer'>Mark Narciso <span className='sm:block hidden'>| Aspiring Software Engineer</span></p>
         </Link>
       </div>
     </nav>
